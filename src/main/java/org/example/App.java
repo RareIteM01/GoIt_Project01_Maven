68c801f922698718d423301c6acedd32014599aa
Nazar <nazar20050716@gmail.com>
2023-09-23 20:19:20 +0200

    created simple class, added dependency

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+//        System.out.println( "Hello World!" );
+        simpleMultiply(12, 56);
+    }
+
+    public static int simpleMultiply (int a, int b) {
+        System.out.println("First number is " + a + " second number is " + b);
+        return a + b;
     }
 }
